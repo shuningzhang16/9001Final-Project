5f5b5afdb1055aa910927c757dbf849c5c550b63
shuningzhang16 <q473248417@gmail.com>
2025-05-20 22:59:26 +0800

    Create deck.py

---
+++ deck.py
@@ -0,0 +1,29 @@
+from flashcard import FlashCard
+import random
+
+class FlashCardDeck:
+    def __init__(self):
+        self.cards = []
+
+    def add_card(self, english: str, chinese: str):
+        self.cards.append(FlashCard(english, chinese))
+
+    def load_default_cards(self):
+        # 可独立维护词库
+        words = [
+            ("apple", "苹果"),
+            ("book", "书"),
+            ("cat", "猫"),
+            ("hello", "你好"),
+            ("water", "水"),
+            ("computer", "计算机"),
+            ("language", "语言")
+        ]
+        for en, zh in words:
+            self.add_card(en, zh)
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def get_cards(self):
+        return self.cards.copy()
